e1278902bbbb8c64e3d832aabb14f9bac0079a27
Surya Jasper <suryajasper2004@gmail.com>
2021-07-17 12:36:17 -0700

    solves unislanded mazes

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -21,5 +21,48 @@ void microMouseServer::studentAI()
  * void foundFinish();
  * void printUI(const char *mesg);
 */
+    //to check if maze is solved, the mouse must turn right and move forward three times consecutively
+    //after three moves and turns, the mouse should have an empty grid spot in front of it, the spot it started on
+    /*
+    static int rightRotation=0; //will retain value each time function is called
+    static bool didMoveForward=false;//indicates if the mouse's last action was moving forward
+    static bool hasNotTurned=false;//indicates whether mouse is moving in a 2x2 square or some other space
+    */
+    static int moves[3] = { -1, -1, -1}; //initialize moves array
+    static int current_move;
+    static int move_selector=0; //count moves executed
 
+    if (!isWallForward()&&isWallLeft()) {
+        moveForward();
+        current_move = 0;
+    }
+    else if (isWallForward()&&isWallLeft()&&isWallRight()) { //dead end condition
+        turnRight();
+        turnRight();
+        moveForward();
+        current_move = 1;
+    }
+    else if (isWallLeft()) {
+        turnRight();
+        moveForward();
+        current_move = 2;
+    }
+    else {
+        turnLeft();
+        moveForward();
+        current_move = 3;
+    }
+    int i = move_selector%3;
+    if (move_selector<3) { //initialize position array
+        moves[i] = current_move;
+    }
+    else {
+        moves[0]=moves[1]; //drop oldest position
+        moves[1]=moves[2]; //shift other two positions 1 index to the left
+        moves[2]=current_move; //append new position to the rightmost index
+    }
+    if ((moves[0]==2)&&(moves[1]==2)&&(moves[2]==2)) { //check if last three moves were (turnRight+moveForward)
+        foundFinish();
+    }
+    move_selector++;
 }
